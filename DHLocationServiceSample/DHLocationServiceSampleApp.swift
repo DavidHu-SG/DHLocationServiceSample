9007aadf4e4b6f29555b4a5cc7fd63c9ee788f29
David Hu <davidhu.sg66@gmail.com>
2023-11-05 11:06:42 +0800

    Initial Commit

---
+++ DHLocationServiceSample/DHLocationServiceSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DHLocationServiceSampleApp.swift
+//  DHLocationServiceSample
+//
+//  Created by David Hu on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DHLocationServiceSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
